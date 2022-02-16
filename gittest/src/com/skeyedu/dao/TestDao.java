eacf28010df76a27961693c623b54f879fa00565
mucheng <1505192297@qq.com>
2022-02-16 19:12:54 +0800

    Java项目第一次提交

---
+++ gittest/src/com/skeyedu/dao/TestDao.java
@@ -0,0 +1,12 @@
+package com.skeyedu.dao;
+
+/**
+ * @author LGC
+ * @date 2022/2/16 19:10
+ */
+
+public class TestDao {
+    public static void main(String[] args) {
+        System.out.println("测试Git");
+    }
+}
